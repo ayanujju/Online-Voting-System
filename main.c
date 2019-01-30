aa2877e076341d041d795026d939ac6b40bde76f
Ayan Raj <44431952+ayanujju@users.noreply.github.com>
2019-01-30 20:14:46 +0530

    Update
    Including main function

---
+++ main.c
@@ -0,0 +1,25 @@
+
+// Including main function
+
+int main()
+{
+
+    system("clear");
+
+    printf("\n\n\n");
+    printf("------------WELCOME TO ONLINE VOTING SYSTEM--------------");
+
+    printf("-------****************************************----  \n\n\n\n\n");
+    printf("\t\t ---------Please Enter One(1) For logging Vote Main Loges -------\n\n\n");
+    int B;
+
+    scanf("%d",&B);
+
+    if(B==1)
+    {
+
+        main_loges();
+    }
+
+    return 0;
+}
